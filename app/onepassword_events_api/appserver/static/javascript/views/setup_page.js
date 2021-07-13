af550aa6c159ffd7a60df99e0ed03d5d45f3b299
Hilary Hacksel <hilary.hacksel@agilebits.com>
2021-07-13 13:49:19 -0400

    Removes redirect so users can read the successs message

--- app/onepassword_events_api/appserver/static/javascript/views/setup_page.js
+++ app/onepassword_events_api/appserver/static/javascript/views/setup_page.js
@@ -7,42 +7,38 @@ const CUSTOM_CONF = "events_reporting";
 const CUSTOM_CONF_STANZA = "config";
 
 export async function perform(splunk_js_sdk, setup_options) {
-	var app_name = "onepassword_events_api";
+  var app_name = "onepassword_events_api";
 
-	var application_name_space = {
-		owner: "nobody",
-		app: app_name,
-		sharing: "app",
-	};
+  var application_name_space = {
+    owner: "nobody",
+    app: app_name,
+    sharing: "app",
+  };
 
-	try {
-		// Create the Splunk JS SDK Service object
-		const splunk_js_sdk_service = Config.create_splunk_js_sdk_service(
-			splunk_js_sdk,
-			application_name_space
-		);
+  try {
+    // Create the Splunk JS SDK Service object
+    const splunk_js_sdk_service = Config.create_splunk_js_sdk_service(
+      splunk_js_sdk,
+      application_name_space
+    );
 
-		// // Get conf and do stuff to it
-		await Splunk.update_configuration_file(
-			splunk_js_sdk_service,
-			CUSTOM_CONF,
-			CUSTOM_CONF_STANZA,
-			setup_options
-		);
+    // // Get conf and do stuff to it
+    await Splunk.update_configuration_file(
+      splunk_js_sdk_service,
+      CUSTOM_CONF,
+      CUSTOM_CONF_STANZA,
+      setup_options
+    );
 
-		// Completes the setup, by access the app.conf's [install]
-		// stanza and then setting the `is_configured` to true
-		await Config.complete_setup(splunk_js_sdk_service);
-
-		// Reloads the splunk app so that splunk is aware of the
-		// updates made to the file system
-		await Config.reload_splunk_app(splunk_js_sdk_service, app_name);
-	} catch (error) {
-		// This could be better error catching.
-		// Usually, error output that is ONLY relevant to the user
-		// should be displayed. This will return output that the
-		// user does not understand, causing them to be confused.
-		console.log("Error:", error);
-		alert("Error:" + error);
-	}
+    // Completes the setup, by access the app.conf's [install]
+    // stanza and then setting the `is_configured` to true
+    await Config.complete_setup(splunk_js_sdk_service);
+  } catch (error) {
+    // This could be better error catching.
+    // Usually, error output that is ONLY relevant to the user
+    // should be displayed. This will return output that the
+    // user does not understand, causing them to be confused.
+    console.log("Error:", error);
+    alert("Error:" + error);
+  }
 }
