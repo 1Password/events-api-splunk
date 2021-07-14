60dca8c99ee34a35aefb8e5db4168625dd0e707a
Anthony Leung <anthony.leung@agilebits.com>
2021-07-14 16:34:50 -0400

    use jwt features instead of introspect endpoint

--- src/main.go
+++ src/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
 	"os"
@@ -14,9 +13,6 @@ import (
 	events "go.1password.io/eventsapi-splunk/api"
 	"go.1password.io/eventsapi-splunk/utils"
 )
-
-const ItemUsageFeatureScope = "itemusages"
-const SignInAttemptsFeatureScope = "signinattempts"
 
 var EventBuildType string // Injected at build time so we can make multiple apps
 type EnvConfig struct {
@@ -31,17 +27,17 @@ type EnvConfig struct {
 // Gets environment variables and normalizes values to EnvConfig.
 // Note that the toml parsing library does not support BOM characters.
 // LoadConfig must trim a BOM prefix before passing the config bytes to the parser.
-func loadConfig() (*EnvConfig, error) {
+func loadConfig() (*EnvConfig, *utils.JWTClaims, error) {
 	log.Println("Loading config")
 	splunkHome := os.Getenv("SPLUNK_HOME")
 	if splunkHome == "" {
-		return nil, fmt.Errorf("SPLUNK_HOME environment variable must be set")
+		return nil, nil, fmt.Errorf("SPLUNK_HOME environment variable must be set")
 	}
 
 	configPath := splunkHome + "/etc/apps/onepassword_events_api/local/events_reporting.conf"
 	configFile, err := os.Open(configPath)
 	if err != nil {
-		return nil, fmt.Errorf("could not open config file: %w", err)
+		return nil, nil, fmt.Errorf("could not open config file: %w", err)
 	}
 	defer configFile.Close()
 
@@ -52,7 +48,7 @@ func loadConfig() (*EnvConfig, error) {
 	}
 	var lc LocalConfig
 	if _, err := toml.DecodeReader(br, &lc); err != nil {
-		return nil, fmt.Errorf("could not decode toml config file: %w", err)
+		return nil, nil, fmt.Errorf("could not decode toml config file: %w", err)
 	}
 	config := lc.Config
 	config.ItemUsageCursorFile = path.Join(splunkHome, config.ItemUsageCursorFile)
@@ -60,7 +56,7 @@ func loadConfig() (*EnvConfig, error) {
 
 	jwt, err := utils.ParseJWTClaims(config.AuthToken)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	url, err := jwt.GetEventsURL()
@@ -70,13 +66,13 @@ func loadConfig() (*EnvConfig, error) {
 		config.Url = url
 	}
 
-	return &config, nil
+	return &config, jwt, nil
 }
 
 func main() {
 	log.Println("Booting...")
 
-	env, err := loadConfig()
+	env, jwt, err := loadConfig()
 	if err != nil {
 		err := fmt.Errorf("could not start: %w", err)
 		panic(err)
@@ -88,15 +84,10 @@ func main() {
 	}
 
 	eventsAPI := events.NewEventsAPI(env.AuthToken, env.Url)
-	res, err := eventsAPI.Introspect(context.TODO())
-	if err != nil {
-		err := fmt.Errorf("introspect request failed: %w", err)
-		panic(err)
-	}
 
-	if utils.ContainsString(SignInAttemptsFeatureScope, res.Features) && EventBuildType == SignInAttemptsFeatureScope {
+	if jwt.HaveSignInAttemptsFeature() && EventBuildType == utils.SignInAttemptsFeatureScope {
 		actions.StartSignIns(env.SignInCursorFile, env.Limit, &env.StartAt, eventsAPI)
-	} else if utils.ContainsString(ItemUsageFeatureScope, res.Features) && EventBuildType == ItemUsageFeatureScope {
+	} else if jwt.HaveItemUsageFeature() && EventBuildType == utils.ItemUsageFeatureScope {
 		actions.StartItemUsages(env.ItemUsageCursorFile, env.Limit, &env.StartAt, eventsAPI)
 	}
 }
